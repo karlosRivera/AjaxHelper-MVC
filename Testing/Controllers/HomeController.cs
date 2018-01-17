7b42bdb8038a4c90fd6854aac7b28de3b6443a07
tridipkolkata <tridip2000@gmail.com>
2018-01-17 20:20:45 +0530

    first commit

---
+++ Testing/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Employee obj)
+        {
+            return PartialView("_EmployeeList", obj);
+        }
+
+        //[HttpPost]
+        public ActionResult PartialForm()
+        {
+            return PartialView("_TestForm");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult BootStrap2ColumnForm()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Testing/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;  
+
+namespace Testing.Models
+{
+    public class Employee
+    {
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }  
+}
